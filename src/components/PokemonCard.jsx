b8ceb2d441ce20a4892aba4ce2507a5d69b5a546
Alexandre-27 <alexandre24102000@gmail.com>
2023-10-10 14:49:29 +0200

    dynamic add in component PokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,18 @@
 function PokemonCard() {
-    return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-            <figcaption>Bulbasaur</figcaption>
-        </figure>
-    );
+    const pokemon = pokemonList[0]
+
+    return <section>{pokemon === pokemonList[0] ? <div><p>bulbasaur</p><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/></div> : <div><p>Mew</p><p>???</p></div>}</section>
 }
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 export default PokemonCard;
