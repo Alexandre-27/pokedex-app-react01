8ccdee82f0cf173afbf774d61cb4ac454efdae1d
Alexandre-27 <alexandre24102000@gmail.com>
2023-10-11 11:09:47 +0200

    Change ternaire

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import PokemonCard from "./components/PokemonCard"
 
+
+
 function App() {
+
   return (
     <div>
       <PokemonCard />
     </div>
   ); 
 }
+
+
 export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,18 +1,25 @@
-function PokemonCard() {
-    const pokemon = pokemonList[0]
-
-    return <section>{pokemon === pokemonList[0] ? <div><p>bulbasaur</p><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/></div> : <div><p>Mew</p><p>???</p></div>}</section>
-}
-
 const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+function PokemonCard() {
+  const pokemon = pokemonList[0]
+  
+    return (
+    <figure>{pokemon.imgSrc ?(<img src={pokemon.imgSrc} 
+      alt={pokemon.name} /> 
+    ) : (
+    <p>???</p>
+    )}
+    <figcaption>{pokemon.name}</figcaption>
+    </figure>
+)}
 
 export default PokemonCard;
