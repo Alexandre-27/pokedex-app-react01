a2a5cdd12c67f25d91a561ec951201f57e3e6968
Alexandre-27 <alexandre24102000@gmail.com>
2023-10-11 11:42:23 +0200

    Problem props resolve

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,18 +1,13 @@
-function PokemonCard() {
-    const pokemon = pokemonList[0]
+function PokemonCard(props) {
 
-    return <section>{pokemon === pokemonList[0] ? <div><p>bulbasaur</p><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/></div> : <div><p>Mew</p><p>???</p></div>}</section>
-}
-
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+    return (
+    <figure>{props.pokemon.imgSrc ?(<img src={props.pokemon.imgSrc} 
+      alt={props.pokemon.name} /> 
+    ) : (
+    <p>???</p>
+    )}
+    <figcaption>{props.pokemon.name}</figcaption>
+    </figure>
+)}
 
 export default PokemonCard;
